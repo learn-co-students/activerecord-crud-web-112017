05c3807c39dcb05a05ea6c449097a3a7eda25389
L-A-Davis <lindsay.amanda.davis@gmail.com>
2017-12-03 18:03:27 -0500

    complete activeRecord CRUD methods and migrations

---
+++ db/migrate/20171203203622_update_datatype_release_date_and_in_theaters.rb
@@ -0,0 +1,6 @@
+class UpdateDatatypeReleaseDateAndInTheaters < ActiveRecord::Migration
+  def change
+    change_column :movies, :release_date, :integer
+    change_column :movies, :in_theaters, :boolean
+  end
+end
